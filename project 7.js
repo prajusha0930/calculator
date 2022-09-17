b56008bbee7ea2104d9821f084d1fbf847208c4b
Prajusha <113427930+prajusha0930@users.noreply.github.com>
2022-09-17 20:08:32 +0530

    Add files via upload

---
+++ project 7.js
@@ -0,0 +1,24 @@
+function add()
+{
+    var result=parseInt(document.getElementById("firstno").value)+
+    parseInt(document.getElementById("secondno").value)
+    window.alert(result);
+}
+function sub()
+{
+    var result=parseInt(document.getElementById("firstno").value)-
+    parseInt(document.getElementById("secondno").value)
+    window.alert(result);
+}
+function mul()
+{
+    var result=parseInt(document.getElementById("firstno").value)*
+    parseInt(document.getElementById("secondno").value)
+    window.alert(result);
+}
+function div()
+{
+    var result=parseInt(document.getElementById("firstno").value)/
+    parseInt(document.getElementById("secondno").value)
+    window.alert(result);
+}
